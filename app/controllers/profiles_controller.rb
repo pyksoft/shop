0d84e08417edeacedc218378b3d499c1e9f69bb7
pyksoft <pyksoft@gmail.com>
2016-11-14 15:48:26 +0100

    working profile actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -33,8 +33,6 @@ class ProfilesController < ApplicationController
     end
 
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :bio)
+      params.require(:profile).permit(:first_name, :last_name )
     end
-end
-
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,6 @@ class Profile < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :bio, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
