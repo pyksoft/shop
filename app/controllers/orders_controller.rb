e58f608f9788245d7d092e6b75ea52f33ab7c610
pyksoft <pyksoft@gmail.com>
2016-11-18 15:36:27 +0100

    fixed error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,25 +6,6 @@ helper_method :sort_column, :sort_direction
     @orders = Order.all
   end
 
-  def set_order_status
-    order.update_attribute(:order_status_id = 1)
-  end
-
-  def update_subtotal
-    self[:subtotal] = subtotal
-  end
-
-  def update_shipping
-    self[:shipping] = shipping
-  end
-
-  def update_tax
-    self[:tax] = tax
-  end
-
-  def update_total
-    self[:total] = total
-  end
 
 
 end
